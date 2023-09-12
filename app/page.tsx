2ea47f33e9733667b57c0ac0042d96e7a5cd2e66
Them Dang <dnthem@gmail.com>
2023-09-11 19:12:07 -0700

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default async function Home() {
   return (
     <main className="flex flex-col min-h-screen w-full items-center justify-center bg-primary">
       <Greeting />
-      <Link href="/download" className="text-blue-500 hover:underline" >Download</Link>
+      <Link href="/download" className="text-blue-500 hover:underline">
+        Download
+      </Link>
       <Container />
     </main>
   )
