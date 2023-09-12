b092a917b15eb321aa1acbda0f126203123d5d4f
Them Dang <dnthem@gmail.com>
2023-09-12 12:42:07 -0700

    update interface for images

--- lib/models/Images.ts
+++ lib/models/Images.ts
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
 export interface IImages extends mongoose.Document {
-  filename: String;
-  type: String;
-  image : String;
+  filename: string;
+  type: string;
+  size: number;
+  image : string;
 }
 
 const ImageSchema = new mongoose.Schema<IImages>({
   filename: String,
   type: String,
+  size: Number,
   image: String, // base64 encoded
 });
 
