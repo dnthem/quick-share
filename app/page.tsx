27053ec0f8e6e5228cec8fb4d5fbee2c4c87ec4b
Them Dang <dnthem@gmail.com>
2023-09-12 12:38:59 -0700

    add small instruction

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ export default async function Home() {
   return (
     <main className="flex flex-col min-h-screen w-full items-center justify-center bg-primary">
       <Greeting />
-      <Link href="/download" className="text-blue-500 hover:underline">
-        Download
-      </Link>
+      <span className="text-white">
+        Just here for the download? &#8594;&nbsp;
+
+        <Link href="/download" className="text-blue-500 hover:underline">
+          Download
+        </Link>
+      </span>
       <Container />
     </main>
   )
