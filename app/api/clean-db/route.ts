23a72fa9ed3d3ad811ea075041022280b25049de
Them Dang <dnthem@gmail.com>
2023-09-22 12:20:18 -0700

    add cron

---
+++ app/api/clean-db/route.ts
@@ -0,0 +1,15 @@
+import dbConnect from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import Images from "@/lib/models/Images";
+import HashTable from "@/lib/models/HashTable";
+
+export async function DELETE() {
+  try {
+    await dbConnect();
+    await Images.deleteMany({});
+    await HashTable.deleteMany({});
+    return NextResponse.json({ message: 'All images deleted' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Something went wrong: ' + error }, { status: 500 });
+  }
+}
