d667e54c5b354b01598f309eb4528767a3498016
Them Dang <dnthem@gmail.com>
2023-09-22 11:56:16 -0700

    add header compt

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header
+      className="bg-primary flex flex-col items-center justify-center text-center py-4"
+    >
+      <h1 className="text-4xl font-bold text-white">QuickShare</h1>
+    </header>
+  )
+}
+
+export default Header
