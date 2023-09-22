fa217239b73e9495407d523c247f62a05fda0240
Them Dang <dnthem@gmail.com>
2023-09-22 11:56:33 -0700

    add header and footer compts

--- components/index.ts
+++ components/index.ts
@@ -3,10 +3,14 @@ import Container from "./Container";
 import Download from "./Download";
 import Greeting from "./Greeting";
 import Loading from "./Loading";
+import Header from "./Header";
+import Footer from "./Footer";
 export {
   Upload,
   Container,
   Download,
   Greeting,
-  Loading
+  Loading,
+  Header,
+  Footer
 };
