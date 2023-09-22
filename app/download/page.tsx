80f1633bfd65512875d5b0827dac4a2a7adf3779
Them Dang <dnthem@gmail.com>
2023-09-22 11:54:20 -0700

    remove download h1

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -5,9 +5,6 @@ import Link from 'next/link'
 function DownloadPage( {searchParams}: any) {
   return (
     <main className="flex flex-col items-center min-h-screen">
-      <h1
-        className='text-4xl font-bold text-center mt-8 mb-4'
-      >Download</h1>
       <Link href="/" className="text-blue-500 hover:underline" >Back to home</Link>
       <Download id={searchParams.id}/>
     </main>
