5b78b837bcf795b011a9f5b8d5e745649a4c51f1
Them Dang <dnthem@gmail.com>
2023-09-22 11:55:44 -0700

    add footer compt

---
+++ components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-primary flex flex-col items-center justify-center text-center">
+      <p className="text-white">
+        Made by{' '}
+        <a
+          href="https://dnthem.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-300 underline"
+        >
+          Them Dang
+        </a>
+        &nbsp; & Hoa Do
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
