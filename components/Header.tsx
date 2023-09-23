c1b6fa0e4f0e1d13ed98ac106eb78df2dbaa63f6
Them Dang <63533605+dnthem@users.noreply.github.com>
2023-09-23 15:05:34 -0700

    add quickshare home link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import Link from 'next/link'
 function Header() {
   return (
     <header
       className="bg-primary flex flex-col items-center justify-center text-center py-4"
     >
-      <h1 className="text-4xl font-bold text-white">QuickShare</h1>
+      <h1 className="text-4xl font-bold text-white">
+        <Link 
+         href="/"
+         >
+        QuickShare
+        </Link>
+        </h1>
     </header>
   )
 }
